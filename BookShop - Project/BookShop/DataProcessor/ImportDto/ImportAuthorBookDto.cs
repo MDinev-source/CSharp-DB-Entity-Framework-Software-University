4d25c8f73e1b96c50eb21a9b96e4ebeded87010c
MDinev-source <milko01@abv.bg>
2021-02-28 17:17:13 +0200

    BookShop project

---
+++ BookShop - Project/BookShop/DataProcessor/ImportDto/ImportAuthorBookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.DataProcessor.ImportDto
+{
+    using Newtonsoft.Json;
+    public class ImportAuthorBookDto
+    {
+        [JsonProperty("Id")]
+        public int? BookId { get; set; }
+    }
+}
---
+++ BookShop - Project/BookShop/DataProcessor/Serializer.cs
@@ -0,0 +1,19 @@
+﻿namespace BookShop.DataProcessor
+{
+    using System;
+
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportMostCraziestAuthors(BookShopContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportOldestBooks(BookShopContext context, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
