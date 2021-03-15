b55a27897fa33f8bf977d2f2ac9ebbb770b5793a
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-03-15 18:45:01 +0200

    Implement Bonus Task

--- SoftJail - Project/SoftJail/SoftJail/DataProcessor/Bonus.cs
+++ SoftJail - Project/SoftJail/SoftJail/DataProcessor/Bonus.cs
@@ -3,12 +3,26 @@
 
     using Data;
     using System;
+    using SoftJail.Data.Models;
 
     public class Bonus
     {
         public static string ReleasePrisoner(SoftJailDbContext context, int prisonerId)
         {
-           throw new NotImplementedException();
+            Prisoner prisoner = context.Prisoners.Find(prisonerId);
+
+            if (prisoner.ReleaseDate == null)
+            {
+                return $"Prisoner {prisoner.FullName} is sentenced to life";
+            }
+
+            prisoner.ReleaseDate = DateTime.Now;
+            prisoner.CellId = null;
+
+            context.Prisoners.Update(prisoner);
+            context.SaveChanges();
+
+            return $"Prisoner {prisoner.FullName} released";
         }
     }
 }
