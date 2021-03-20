39ec6452d8f2089b51d7d307db89672b61262011
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-03-20 18:31:14 +0200

    Starting Data Import

---
+++ VaporStore - Project/VaporStore/VaporStore/DataProcessor/Dto's/ImportDto's/ImportGameDto.cs
@@ -0,0 +1,28 @@
+﻿
+namespace VaporStore.DataProcessor.Dto_s.ImportDto_s
+{
+using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public class ImportGameDto
+    {
+        [JsonProperty ("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("ReleaseDate")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("Developer")]
+        public string Developer { get; set; }
+
+        [JsonProperty("Genre")]
+        public string Genre { get; set; }
+        
+        [JsonProperty("Tags")]
+        public ICollection<string> Tags { get; set; }
+
+    }
+}
