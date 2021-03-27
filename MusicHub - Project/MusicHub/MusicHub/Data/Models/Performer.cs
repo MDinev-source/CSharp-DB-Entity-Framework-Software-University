d18d24888fe6a565ae7e28a90c6d83bef0fb9d35
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-03-27 09:40:28 +0200

    New Project MusicHub
    Model Definition Almost Done

---
+++ MusicHub - Project/MusicHub/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,32 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class Performer
+    {
+        public Performer()
+        {
+            this.PerformerSong = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(18, 70)]
+        public int Age { get; set; }
+
+        [Range(typeof(decimal), "0.00", "79228162514264337593543950335")]
+        public decimal NetWorth { get; set; }
+
+        public ICollection<SongPerformer> PerformerSong { get; set; }
+    }
+}
