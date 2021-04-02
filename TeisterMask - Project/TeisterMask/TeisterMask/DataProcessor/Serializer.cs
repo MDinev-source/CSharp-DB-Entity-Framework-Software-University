77dfa57eef690612a32a18958a7de800ecc44107
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-04-02 20:00:14 +0300

    Export Json Work

--- TeisterMask - Project/TeisterMask/TeisterMask/DataProcessor/Serializer.cs
+++ TeisterMask - Project/TeisterMask/TeisterMask/DataProcessor/Serializer.cs
@@ -1,19 +1,49 @@
 ﻿namespace TeisterMask.DataProcessor
 {
     using System;
+    using System.Globalization;
+    using System.Linq;
     using Data;
+    using Newtonsoft.Json;
+    using TeisterMask.Data.Models;
     using Formatting = Newtonsoft.Json.Formatting;
 
     public class Serializer
     {
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+            var employees = context.Employees
+                .Where(x => x.EmployeesTasks.Any(d => d.Task.OpenDate >= date))
+                .OrderByDescending(e => e.EmployeesTasks.Count(t => t.Task.OpenDate >= date))
+                .ThenBy(e => e.Username)
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                    .Where(t => t.Task.OpenDate >= date)
+                    .Select(t => new
+                    {
+                        TaskName = t.Task.Name,
+                        OpenDate = t.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = t.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = t.Task.LabelType.ToString(),
+                        ExecutionType = t.Task.ExecutionType.ToString()
+                    })
+                    .OrderByDescending(x => DateTime.ParseExact(x.DueDate, "d", CultureInfo.InvariantCulture))
+                    .ThenBy(x => x.TaskName)
+                    .ToList()
+                })
+                .Take(10)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+            return json;
+        }
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
             throw new NotImplementedException();
         }
 
-        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
